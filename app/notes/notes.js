a0bdca4887cb8b9a0c0e7cffda0f832e897ef4ed
Zach <millikanzach@gmail.com>
2016-06-14 13:46:39 -0400

    wrapped notes.js in a function and moved NotesController and config to named functions.

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,37 +1,38 @@
-angular.module('meganote.notes', [
-  'ui.router'
-])
+(function() {angular.module('meganote.notes', ['ui.router'])
+  .config(notesConfig)
+  .controller('NotesController', NotesController);
 
-.config(function($stateProvider) {
-  $stateProvider
+  function notesConfig($stateProvider) {
+    $stateProvider
 
-  .state('notes', {
-    url: '/notes',
-    templateUrl: 'notes/notes.html',
-    controller: 'NotesController'
-  })
+    .state('notes', {
+      url: '/notes',
+      templateUrl: 'notes/notes.html',
+      controller: 'NotesController'
+    })
 
-  .state('notes.form', {
-    url: '/:noteId',
-    templateUrl: 'notes/notes-form.html'
-  });
-})
-
-.controller('NotesController', function($scope) {
-  $scope.notes = [];
-  $scope.note = {};
-  // $scope.message = "seperate controller";
-  $scope.saveNote = function() {
-          $scope.notes.push($scope.note);
-          $scope.note = {};
+    .state('notes.form', {
+      url: '/:noteId',
+      templateUrl: 'notes/notes-form.html'
+    });
   }
 
-  $scope.removeNote = function(index) {
-    $scope.notes.splice(index, 1);
-  }
+  function NotesController($scope) {
+    $scope.notes = [];
+    $scope.note = {};
+    // $scope.message = "seperate controller";
+    $scope.saveNote = function() {
+            $scope.notes.push($scope.note);
+            $scope.note = {};
+    }
 
-  $scope.editNote = function (note) {
-          // $scope.editing = true;
-          $scope.note = note;
-        }
-});
+    $scope.removeNote = function(index) {
+      $scope.notes.splice(index, 1);
+    }
+
+    $scope.editNote = function (note) {
+            // $scope.editing = true;
+            $scope.note = note;
+          }
+  };
+})();
