67f91c100b33b3e39b644ffcb361a0e05bc4be7d
Zach <millikanzach@gmail.com>
2016-06-15 11:48:40 -0400

    added delete

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,7 +1,7 @@
 (function() {
   angular.module('meganote.notes', ['ui.router'])
-  .config(notesConfig)
-  .controller('NotesController', NotesController);
+    .config(notesConfig)
+    .controller('NotesController', NotesController);
 
   notesConfig.$inject = ['$stateProvider'];
   function notesConfig($stateProvider) {
@@ -19,7 +19,7 @@
     });
   }
 
-  NotesController.$inject = ['$state', '$scope', 'NotesService']
+  NotesController.$inject = ['$state', '$scope', 'NotesService'];
   function NotesController($state, $scope, NotesService) {
     $state.go('notes.form');
 
@@ -28,29 +28,33 @@
         $scope.notes = NotesService.notes;
       });
 
-
     $scope.clearForm = function() {
-      $scope.note = { title: '', body_html: ''};
+      $scope.note = { title: '', body_html: '' };
     };
 
-    $scope.note = {};
-    // $scope.message = "seperate controller";
-    $scope.saveNote = function() {
+    $scope.save = function() {
       if ($scope.note._id) {
         NotesService.update($scope.note);
       }
       else {
-        NotesService.create($scope.note);
+        NotesService.create($scope.note)
+          .then(function(res) {
+            $scope.note = res.data.note;
+          });
       }
-        // $scope.clearForm();
+    };
 
-    }
-    $scope.editNote = function (note) {
-            // $scope.editing = true;
+    $scope.edit = function(note) {
       $scope.note = angular.copy(note);
-    }
-    $scope.removeNote = function(index) {
-      $scope.notes.splice(index, 1);
-    }
-  };
-})();
+    };
+
+    $scope.delete = function() {
+      NotesService.delete($scope.note)
+        .then(function() {
+          $scope.clearForm();
+        });
+    };
+
+    $scope.clearForm();
+  }
+}());
