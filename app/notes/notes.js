5bef07b963fc0fc377d047a69b841f63920c2018
Zach <millikanzach@gmail.com>
2016-07-05 15:41:02 -0400

    Rewrite NotesService using factory syntax.

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -10,64 +10,26 @@
     .state('notes', {
       url: '/notes',
       templateUrl: 'notes/notes.html',
-      controller: 'NotesController'
+      controller: 'NotesController',
+      resolve: {
+        notesLoaded: notesLoaded
+      }
     })
 
     .state('notes.form', {
       url: '/:noteId',
-      templateUrl: 'notes/notes-form.html'
+      templateUrl: 'notes/notes-form.html',
+      controller: 'NotesFormController'
     });
   }
 
-  NotesController.$inject = ['$state', '$scope', 'Flash', 'NotesService'];
-  function NotesController($state, $scope, Flash, NotesService) {
-    $state.go('notes.form');
+  notesLoaded.$inject = ['NotesService'];
+  function notesLoaded(NotesService) {
+    return NotesService.getNotes();
+  }
 
-    NotesService.getNotes()
-      .then(function() {
-        $scope.notes = NotesService.notes;
-      });
-
-    $scope.clearForm = function() {
-      $scope.note = { title: '', body_html: '' };
-    };
-
-    $scope.save = function() {
-      if ($scope.note._id) {
-        NotesService.update($scope.note)
-          .then(
-            function(res) {
-            $scope.note = res.data.note;
-            Flash.create('success', 'It worked!');
-          },
-          function() {
-            Flash.create('danger', 'Ooops something went wrong.');
-          });
-      }
-      else {
-        NotesService.create($scope.note)
-          .then(
-            function(res) {
-            $scope.note = res.data.note;
-            Flash.create('success', 'It worked!');
-          },
-          function() {
-            Flash.create('danger', 'Ooops something went wrong.');
-          });
-      }
-    };
-
-    $scope.edit = function(note) {
-      $scope.note = angular.copy(note);
-    };
-
-    $scope.delete = function() {
-      NotesService.delete($scope.note)
-        .then(function() {
-          $scope.clearForm();
-        });
-    };
-
-    $scope.clearForm();
+  NotesController.$inject = ['$scope', 'NotesService'];
+  function NotesController($scope, NotesService) {
+    $scope.notes = NotesService.notes;
   }
 }());
--- app/notes/notes.service.js
+++ app/notes/notes.service.js
@@ -1,24 +1,38 @@
 (function() {
   angular.module('meganote.notes')
-    .service('NotesService', NotesService);
+    .factory('NotesService', NotesService);
 
   NotesService.$inject = ['$http'];
   function NotesService($http) {
-    var service = this;
-    service.notes = [];
+    var service = {
+      notes: [],
+      getNotes: getNotes,
+      create: create,
+      update: update,
+      destroy: destroy,
+      removeById: removeById,
+      find: find,
+    };
 
-    service.getNotes = function() {
-      var notesPromise = $http.get('https://meganote.herokuapp.com/notes');
+    return service;
+
+    ///////////////////////////////////////
+
+
+    // service.notes = [];
+
+    function getNotes () {
+      var notesPromise = $http.get('http://localhost:3030');
 
       notesPromise.then(function(res) {
         service.notes = res.data;
       });
 
       return notesPromise;
-    };
+    }
 
-    service.create = function(note) {
-      var notesPromise = $http.post('https://meganote.herokuapp.com/notes', {
+    function create(note) {
+      var notesPromise = $http.post('http://localhost:3030', {
         note: note
       });
 
@@ -27,39 +41,47 @@
       });
 
       return notesPromise;
-    };
+    }
 
-    service.update = function(note) {
-      var notesPromise = $http.put('https://meganote.herokuapp.com/notes/' + note._id, {
+    function update(note) {
+      var notesPromise = $http.put('http://localhost:3030/' + note._id, {
         note: note
       });
 
       notesPromise.then(function(res) {
-    
+
         service.removeById(res.data.note._id);
         service.notes.unshift(res.data.note);
 
       });
 
       return notesPromise;
-    };
+    }
 
-    service.delete = function(note) {
-      var notesPromise = $http.delete('https://meganote.herokuapp.com/notes/' + note._id);
+    function destroy(note) {
+      var notesPromise = $http.delete('http://localhost:3030/' + note._id);
 
       notesPromise.then(function(res) {
         service.removeById(res.data.note._id);
       });
 
       return notesPromise;
-    };
+    }
 
-    service.removeById = function(id) {
+    function removeById (id) {
       for (var i=0; i < service.notes.length; i++) {
         if (service.notes[i]._id === id) {
           return service.notes.splice(i, 1);
         }
       }
-    };
+    }
+
+    function find(id) {
+      for (var i=0; i < service.notes.length; i++) {
+        if (service.notes[i]._id === id) {
+          return angular.copy(service.notes[i]);
+        }
+      }
+    }
   }
 }());
