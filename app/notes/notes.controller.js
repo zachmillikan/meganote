8d9ade4eae9de0f7dac559492067281ff20efb08
Zach <millikanzach@gmail.com>
2016-07-06 13:56:14 -0400

    Refactor NotesController using controllerAs

--- app/notes/notes.controller.js
+++ app/notes/notes.controller.js
@@ -1,9 +1,11 @@
 (function() {
+  'use strict';
   angular.module('meganote.notes')
     .controller('NotesController', NotesController);
 
   NotesController.$inject = ['$scope', 'NotesService'];
   function NotesController($scope, NotesService) {
-    $scope.notes = NotesService.notes;
+    var vm = this;
+    vm.notes = NotesService.notes;
   }
 }());
